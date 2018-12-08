2e464e64746f686e09c2f097756d8b7437f3969a
michael1011 <me@michael1011.at>
2018-12-08 12:33:03 +0100

    feat: export more reusable methods and objects (#2)

--- lib/Boltz.ts
+++ lib/Boltz.ts
@@ -1,11 +1,22 @@
+import Networks from './consts/Networks';
+import { OutputType } from './consts/Enums';
+import { ScriptElement, TransactionOutput } from './consts/Types';
+import * as Scripts from './swap/Scripts';
 import { pkRefundSwap, pkHashRefundSwap } from './swap/Swap';
-import { constructClaimTransaction } from './swap/Claim';
-import { constructRefundTransaction } from './swap/Refund';
 import { detectSwap } from './swap/SwapDetector';
 import { estimateFee } from './FeeCalculator';
+import { constructClaimTransaction } from './swap/Claim';
+import { constructRefundTransaction } from './swap/Refund';
 import * as SwapUtils from './swap/SwapUtils';
 
 export {
+  Networks,
+  OutputType,
+  ScriptElement,
+  TransactionOutput,
+
+  Scripts,
+
   pkRefundSwap,
   pkHashRefundSwap,
 
