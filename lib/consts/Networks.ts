60e0fbebc425de9389d26c36f4a24f36c730eff9
michael1011 <me@michael1011.at>
2019-10-08 17:58:39 +0200

    feat: add Dogecoin network values (#29)

--- lib/consts/Networks.ts
+++ lib/consts/Networks.ts
@@ -2,15 +2,16 @@ import { networks } from 'bitcoinjs-lib';
 
 const bitcoinPrefix = '\\x18Bitcoin Signed Message:\n';
 const litecoinPrefix = '\\x19Litecoin Signed Message:\n';
+const dogecoinPrefix = '\\x19Dogecoin Signed Message:\n';
 
 const testnetBip32 = {
-  private: 0x019D9CFE,
   public: 0x019DA462,
+  private: 0x019D9CFE,
 };
 
 const simnetBip32 = {
-  private: 0x488B21E,
   public: 0x420BD3A,
+  private: 0x488B21E,
 };
 
 const Networks = {
@@ -61,6 +62,32 @@ const Networks = {
     bech32: 'rltc',
     scriptHash: 0x3a,
     pubKeyHash: 0x6f,
+    wif: 0xef,
+  },
+
+  // Dogecoin networks
+  dogecoinMainnet: {
+    messagePrefix: dogecoinPrefix,
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398,
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e,
+  },
+  dogecoinTestnet: {
+    messagePrefix: dogecoinPrefix,
+    bip32: testnetBip32,
+    pubKeyHash: 0x71,
+    scriptHash: 0xc4,
+    wif: 0xf1,
+  },
+  dogecoinRegtest: {
+    messagePrefix: dogecoinPrefix,
+    bip32: testnetBip32,
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
     wif: 0xef,
   },
 };
