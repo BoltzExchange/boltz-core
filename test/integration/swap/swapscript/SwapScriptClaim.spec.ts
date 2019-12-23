a2ab7587497d11b5685ad074f6e150a2da5752e0
michael1011 <me@michael1011.at>
2019-12-23 13:47:48 +0100

    tests: add tests for failing claim transactions

--- test/integration/swap/swapscript/SwapScriptClaim.spec.ts
+++ test/integration/swap/swapscript/SwapScriptClaim.spec.ts
@@ -1,8 +1,27 @@
+import { randomBytes } from 'crypto';
 import { claimDetails } from './SwapScript.spec';
 import { constructClaimTransaction } from '../../../../lib/Boltz';
 import { bitcoinClient, destinationOutput, claimSwap } from '../Utils';
 
 describe('SwapScript claim', () => {
+  test('should not claim swaps if the preimage has an invalid hash', async () => {
+    let actualError: any;
+
+    try {
+      const toClaim = {
+        ...claimDetails[0],
+      };
+      toClaim.preimage = randomBytes(32);
+
+      await claimSwap(toClaim);
+    } catch (error) {
+      actualError = error;
+    }
+
+    expect(actualError.code).toEqual(-26);
+    expect(actualError.message).toEqual('mandatory-script-verify-flag-failed (Locktime requirement not satisfied) (code 16)');
+  });
+
   test('should claim a P2WSH swap', async () => {
     await claimSwap(claimDetails[0]);
   });
