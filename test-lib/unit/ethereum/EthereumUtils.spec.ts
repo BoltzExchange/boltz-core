d0388526f4dd8236cba38627188bf86f0757903e
michael1011 <me@michael1011.at>
2020-10-26 16:18:41 +0100

    fix: remove export that requires ethers.js

--- test-lib/unit/ethereum/EthereumUtils.spec.ts
+++
@@ -1,9 +0,0 @@
-import { fromEventAddress } from '../../../lib/ethereum/EthereumUtils';
-
-describe('EthereumUtils', () => {
-  test('should convert from event topic to checksum address', () => {
-    expect(
-      fromEventAddress('0x00000000000000000000000017ec8597ff92c3f44523bdc65bf0f1be632917ff')
-    ).toEqual('0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff');
-  });
-});
