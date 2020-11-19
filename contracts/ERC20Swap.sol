cf5cc5b71f1b60a28cea43fbf94916e544004287
michael1011 <me@michael1011.at>
2020-11-19 13:04:45 +0100

    chore: update Solidity to v0.7.5 (#54)

--- contracts/ERC20Swap.sol
+++ contracts/ERC20Swap.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity 0.7.4;
+pragma solidity 0.7.5;
 
 import "./TransferHelper.sol";
 
--- contracts/TestERC20.sol
+++ contracts/TestERC20.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.4;
+pragma solidity 0.7.5;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
