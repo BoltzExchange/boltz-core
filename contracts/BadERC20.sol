8710c5a53830a3553553451c2c8b98bedc59a336
michael1011 <me@michael1011.at>
2024-05-25 22:36:51 +0200

    chore: bump Solidity compiler to v0.8.26

--- contracts/BadERC20.sol
+++ contracts/BadERC20.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity ^0.8.26;
 
 // This token is intentionally not complying to the ERC20 standard so that the ERC20Swap contract
 // can be tested against such non-standardised tokens
