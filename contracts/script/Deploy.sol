e8cee268267497fd556fcaeb8dfa35b1736375b9
michael1011 <me@michael1011.at>
2025-08-22 20:24:09 +0200

    chore: deploy Router in script

--- contracts/script/Deploy.sol
+++ contracts/script/Deploy.sol
@@ -6,15 +6,20 @@ import {Script, console} from "forge-std/Script.sol";
 import {EtherSwap} from "../EtherSwap.sol";
 import {ERC20Swap} from "../ERC20Swap.sol";
 import {TestERC20} from "../TestERC20.sol";
+import {Router} from "../Router.sol";
 
 contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
-        console.log("Deployed EtherSwap: ", address(new EtherSwap()));
+        address etherSwapAddress = address(new EtherSwap());
+        console.log("Deployed EtherSwap: ", etherSwapAddress);
         console.log("Deployed ERC20Swap: ", address(new ERC20Swap()));
 
         uint8 decimals = 18;
         TestERC20 erc20 = new TestERC20("TestERC20", "TRC20", decimals, 1000 * (10 ** decimals));
+
+        console.log("Deployed Router: ", address(new Router(etherSwapAddress)));
+
         vm.stopBroadcast();
 
         console.log("Deployed TestERC20: ", address(erc20));
