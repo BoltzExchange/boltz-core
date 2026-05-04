e741cdb3aaead1a70a16d352de7f4c728806354b
michael1011 <me@michael1011.at>
2026-05-04 14:51:25 +0200

    chore: remove TypeChain
    Not needed anymore because we are switching away from ethers

--- typechain.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { execSync } = require('node:child_process');
-
-for (const contract of ['ERC20', 'Router', 'EtherSwap', 'ERC20Swap']) {
-  execSync(
-    `npx typechain --target ethers-v6 --out-dir typechain/ ./out/${contract}.sol/${contract}.json`,
-  );
-}
